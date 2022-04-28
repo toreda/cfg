7fa51b0e278114a89d708f65b9865c29087e5a8b
Cat Whitmer <catwhitmer@gmail.com>
2022-04-28 14:05:11 -0400

    changed the key type

--- src/event.ts
+++ src/event.ts
@@ -1,9 +1,11 @@
+import {Text, textMake} from '@toreda/strong-types';
+
 export class Event {
-	public readonly key;
+	public readonly key: Text;
 	public readonly value: unknown;
 
 	constructor() {
-		this.key = this.key;
+		this.key = textMake('');
 		this.value = this.value;
 	}
 }
